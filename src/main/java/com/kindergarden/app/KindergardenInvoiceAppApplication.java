3b356053dcee6123f0d48d28666af16c245efe02
TSieduszewski <62184872+TSieduszewski@users.noreply.github.com>
2022-07-02 08:42:01 +0200

    Initial commit

---
+++ src/main/java/com/kindergarden/app/KindergardenInvoiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.kindergarden.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergardenInvoiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindergardenInvoiceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kindergarden/app/KindergardenInvoiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kindergarden.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergardenInvoiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
